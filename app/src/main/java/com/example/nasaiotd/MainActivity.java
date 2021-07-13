c416ef45e7afcc51af349cf7c7fb96891e8cee66
gecodes <graham.everitt@gmail.com>
2021-07-13 13:35:20 -0400

    Opened branch and commented req's for first milestone.

--- app/src/main/java/com/example/nasaiotd/MainActivity.java
+++ app/src/main/java/com/example/nasaiotd/MainActivity.java
@@ -4,6 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// Milestone 1 -
+// July 19th 11:59PM (Optional): Both team member submits a pdf. The Github repository
+// needs to have a branch called milestone_1 containing the code for milestone_1.
+
+// Milestone number and date  	Requirements implemented 	  Bonus Marks available
+// 1) July 19th1,                  2, 3, 11, 13                5
+
+// 2.  The project must have at least 1 progress bar and at least 1 button.
+
+// 3.  The project must have at least 1 edit text with appropriate text input method and at least 1
+//     Toast and 1 Snackbar.
+
+// 11. All activities must be integrated into a single working application, on a single emulator, and
+//     must be uploaded to GitHub.
+
+// 13. The functions and variables you write must be properly documented using JavaDoc comments.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
